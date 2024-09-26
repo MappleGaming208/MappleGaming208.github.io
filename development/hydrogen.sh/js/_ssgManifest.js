9cf9776a77eb0a7c762810167708e4627d4a164d
MappleGaming208 <122428938+MappleGaming208@users.noreply.github.com>
2024-09-25 19:33:01 -0600

    Hydrogen homepage

---
+++ development/hydrogen.sh/js/_ssgManifest.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+self.__SSG_MANIFEST=new Set,self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB();
+
+}
+/*
+     FILE ARCHIVED ON 17:55:37 Feb 24, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:23:29 Sep 26, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.691
+  exclusion.robots: 0.027
+  exclusion.robots.policy: 0.012
+  esindex: 0.014
+  cdx.remote: 11.231
+  LoadShardBlock: 64.929 (3)
+  PetaboxLoader3.datanode: 75.113 (4)
+  load_resource: 66.525
+  PetaboxLoader3.resolve: 52.51
+*/
