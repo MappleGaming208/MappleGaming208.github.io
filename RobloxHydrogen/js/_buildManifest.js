afc69aaed00046e00cd28cd3d212633c4ff9f06b
MappleGaming208 <122428938+MappleGaming208@users.noreply.github.com>
2024-09-25 19:40:17 -0600

    Add files via upload

---
+++ RobloxHydrogen/js/_buildManifest.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/238-e4865488aa09ac37.js","static/chunks/pages/index-b1a7b0aadda0fa34.js"],"/404":["static/chunks/pages/404-6ef68c6e62e8e8dc.js"],"/_docs":["static/chunks/pages/_docs-6e24d49286e47561.js"],"/_error":["static/chunks/pages/_error-a1e5d23d1c19d204.js"],"/billing/checkout-success":["static/chunks/pages/billing/checkout-success-33e677a94659d139.js"],"/download":["static/chunks/738-da6f8af2ecf4d96c.js","static/chunks/pages/download-f0b03bfadfbec8b1.js"],"/verify/[token]":["static/chunks/pages/verify/[token]-d8f65dbfc2cf6459.js"],sortedPages:["/","/404","/_app","/_docs","/_error","/billing/checkout-success","/download","/verify/[token]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
+
+}
+/*
+     FILE ARCHIVED ON 17:55:37 Feb 24, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:23:34 Sep 26, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.745
+  exclusion.robots: 0.035
+  exclusion.robots.policy: 0.021
+  esindex: 0.016
+  cdx.remote: 21.926
+  LoadShardBlock: 256.147 (3)
+  PetaboxLoader3.resolve: 302.988 (4)
+  PetaboxLoader3.datanode: 4386.107 (5)
+  load_resource: 4494.549 (2)
+*/
